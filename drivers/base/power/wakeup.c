105a4c5b1a2d1369adc097f0bd2b4f2f9112ae23
franciscofranco <franciscofranco.1990@gmail.com>
2015-12-03 23:14:42 +0000

    wakeup: add toggles for wlan wakelocks. They are all enabled by default, it's up to the user and I provide no support if Wi-Fi stops working normally without these locks enabled. This is for advanced users
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -23,6 +23,13 @@
 static bool enable_msm_hsic_ws = false;
 module_param(enable_msm_hsic_ws, bool, 0644);
 
+static bool enable_wlan_rx_wake_ws = true;
+module_param(enable_wlan_rx_wake_ws, bool, 0644);
+static bool enable_wlan_ctrl_wake_ws = true;
+module_param(enable_wlan_ctrl_wake_ws, bool, 0644);
+static bool enable_wlan_wake_ws = true;
+module_param(enable_wlan_wake_ws, bool, 0644);
+
 #ifdef CONFIG_BOEFFLA_WL_BLOCKER
 #include "boeffla_wl_blocker.h"
 
@@ -427,6 +434,15 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 		pr_info("wakeup source msm_hsic_host activate skipped\n");
 		return;
 	}
+
+	if (!enable_wlan_rx_wake_ws && !strcmp(ws->name, "wlan_rx_wake"))
+                return;
+
+	if (!enable_wlan_ctrl_wake_ws && !strcmp(ws->name, "wlan_ctrl_wake"))
+                return;
+
+	if (!enable_wlan_wake_ws && !strcmp(ws->name, "wlan_wake"))
+                return;
 
 	ws->active = true;
 	ws->active_count++;
